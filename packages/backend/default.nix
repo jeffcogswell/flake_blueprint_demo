04ff4dcc17026f446d5dc608348a951804a252d7
Jeffrey Cogswell <jeffcogs@gmail.com>
2025-03-16 23:12:10 +0000

    init

---
+++ packages/backend/default.nix
@@ -0,0 +1,16 @@
+{ pkgs }:
+pkgs.mkShell {
+  # Add build dependencies
+  packages = [
+    pkgs.nodejs_23
+    pkgs.geany
+  ];
+
+  # Add environment variables
+  env = { };
+
+  # Load custom bash code
+  shellHook = ''
+
+  '';
+}
